a13b5bcf7436d81f92d5a5cdc31c91e90f4f8ef7
DrCiroSmith <roisasha@gmail.com>
2025-12-30 19:46:54 -0500

    Update to correct Supabase anon key

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://sjfvsxqifojgjmipxlio.supabase.co';
-const supabaseKey = 'sb_publishable_C_5jaw7dltX8JRTtmSLxjw_QeHb9PgU';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqZnZzeHFpZm9qZ2ptaXB4bGlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMzE2NDYsImV4cCI6MjA4MjYwNzY0Nn0.-guchORPkh7c1_P8L7_c07Kx-Zy0eGI9MbVUa_SasBs';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
